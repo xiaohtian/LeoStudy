f13d5e24177dfa2c1d6db33a93fb6ed8f0842dec
LeoHuang <xiaohtian@163.com>
2015-06-18 11:24:17 +0800

    Wechat

---
+++ LeoStudy.Web/Models/WeChatRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeoStudy.Web.Models
+{
+    public class WeChatRequestModel
+    {
+        public string Signature { get; set; }
+        public string Timestamp { get; set; }
+        public string Nonce { get; set; }
+        public string Echostr { get; set; }
+    }
+}
